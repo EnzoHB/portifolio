8354eaced8df65f4a53a352ca58ba18428d06f0a
EnzoHB <EnzoHHB@gmail.com>
2023-01-28 09:13:12 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,74 @@
+<script>
+	import { onMount } from "svelte";
+    import Profile from "./Profile.svelte";
+
+    onMount(() => window.onload = () => {
+        document.title = 'Portifolio';
+        document.querySelector('#favicon').href = '/thumbnail_filled.png';
+    })
+
+    // --------- Header --------------- //  
+
+    let header;
+    let sticky;
+
+    onMount(() => window.onscroll = () => {
+        
+        if (window.pageYOffset > header.offsetTop)
+            return sticky = 'sticky';
+            return sticky = '';
+
+    });
+</script>
+
+<div class=container>
+    <div bind:this={header} class=header class:sticky>
+        <h1>Portifolio</h1>
+    </div>
+    <div class=profiles>
+        <Profile file=forma amount=49 name='Forma Academia Fitness' account=formaacademiabady followers=1344/>
+        <Profile file=vida-leve amount=9 name='Vida Leve' account=vidalevefranquia followers='14.8 mil'/>
+        <Profile file=informatica amount=31 name='Universo Informatica' account=universoinformaticarp followers=2200/>
+        <Profile file=criativa amount=49 name='Papelaria Criativa' account=papelariacriativa_rioptero followers=5106/>        
+    </div>
+</div>
+
+<style>
+    .header {
+        background-color: rgb(255, 252, 247);
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+        width: 100%;
+        height: 10vh;
+
+        display: grid;
+        place-items: center;
+
+        margin-bottom: 5vh;
+
+        font-size: 1vw;
+    }
+
+    .profiles {
+        display: flex;
+        flex-direction: column;
+        gap: 50px;
+
+        overflow: hidden;
+    }
+
+    .container {
+        margin-bottom: 30px;
+    }
+    
+    .sticky {
+        position: fixed;
+    }
+
+    /* Select the .profiles element immediately positioned after the .sticky element */
+    .sticky + .profiles {
+        padding-top: 15vh;
+    }
+
+</style>
+
---
+++ src/Profile.svelte
@@ -0,0 +1,125 @@
+<script>
+
+    export let file;
+    export let amount; 
+    
+    export let name = 'Enzo'
+    export let followers = 326
+    export let account = 'enzohosi';
+
+    function random() {
+        return Math.floor(Math.random() * 10 ** 3)
+    };
+
+    let images = [];
+
+    for (let i = 0; i < amount; i++) {
+        images.push(`./static/profiles/${file}/${i}.png`);
+    };
+
+
+</script>
+
+<div class=container>
+    <div class=info>
+        <div class=icon>
+            <img src='./static/profiles/{file}/profile.jpg' alt='Enzo'>
+        </div>
+        <div class=social>
+            <div style='font-weight: 700'>{name} &centerdot {followers} seguidores</div>
+            <div>@{account}</div>
+        </div>
+    </div>
+    <div class=wrapper>
+        <div class=media>
+            {#each images as image} 
+                <img alt="" width=500 height=500 src={image}>
+            {/each}
+        </div>
+    </div>
+</div>  
+
+<style> 
+    .container {
+        box-sizing: border-box;
+
+        padding-left: 20px;
+        padding-right: 20px;
+
+        width: 100%;
+        gap: 30px;
+
+        display: flex;
+        flex-direction: column;
+    }
+
+    .icon {
+        width: 100px;
+        aspect-ratio: 1;
+        
+        background-color: transparent;
+        border-radius: 50%;
+
+        display: grid;
+        place-items: center;
+    }
+
+    .icon > img {
+        width: 100%;
+        aspect-ratio: 1;
+        background-size: cover;
+    }
+
+    .info {
+        display: flex;
+        gap: 20px;
+
+        font-size: 1.5vw;
+    }
+
+    .social {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    img {
+        border-radius: 10px;
+    }
+
+    .media {
+        width: 100%;
+
+        display: flex;
+        gap: 20px;
+
+        padding-bottom: 20px;
+
+        overflow-y: hidden;
+        overflow-x: scroll;
+    }
+
+
+
+    /* width */
+    ::-webkit-scrollbar {
+      height: 10px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+      background: #f1f1f1;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      background: #888;
+      width: 5px;
+      border-radius: 20px;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+      background: #555;
+    }
+</style>
